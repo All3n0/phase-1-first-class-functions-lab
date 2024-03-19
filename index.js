718b39f9cc300f4e959ee3d629c467db830af2ae
ALLAN <allankipr6@gmail.com>
2024-03-19 16:04:38 +0300

    muki

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+
+const returnFirstTwoDrivers= function lol(arg){
+    arg=['Antonia','Nuru','Amari','Mo']
+return arg.slice(0,2);
+
+}
+const returnLastTwoDrivers=function (arg){
+    arg=['Antonia','Nuru','Amari','Mo']
+    return arg.slice(-2);
+}
+const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers];
+const createFareMultiplier=function(multiplyvalue){
+    return function kiki(mym){
+        return multiplyvalue * mym;
+    }
+    
+}  
+const fareDoubler=function doublefare(double){
+    return double * 2;
+
+}
+const fareTripler=function tripplefare(triple){
+    return triple * 3;
+}
+const selectDifferentDrivers= function (arg,cok){
+    return cok(arg);
+}
